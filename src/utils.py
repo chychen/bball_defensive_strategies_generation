5b6e6ec958fe075f7db79aabfb412d783b64dd53
Chieh Yu Chen <jay_case@hotmail.com>
2017-10-15 13:17:00 +0800

    complete utils

--- src/utils.py
+++ src/utils.py
@@ -9,55 +9,37 @@ import numpy as np
 
 
 class Norm(object):
+    """singletance pattern
+    """
     __instance = None
 
     def __new__(clz, real_data=None):
         if not Norm.__instance:
             Norm.__instance = object.__new__(clz)
-            print("__new__")
+        else:
+            print("Instance Exists! :D")
         return Norm.__instance
 
     def __init__(self, real_data=None):
-        print("__init__")
+        """
+        params
+        ------
+        real_data : float, shape=[#, length=100, players=11, features=4]
+
+        note
+        ----
+        feature :
+            x, y, z, and player position
+        """
         if real_data is not None:
             self.__real_data = real_data
             self.__basket_left = [4, 25]
             self.__basket_right = [90, 25]
             self.__norm_dict = {}
-            # X
-            mean_x = np.mean(
-                real_data[:, :, [0, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]])
-            stddev_x = np.std(
-                real_data[:, :, [0, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]])
-            real_data[:, :, [0, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]] = (
-                real_data[:, :, [0, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]] - mean_x) / stddev_x
-            self.__basket_left[0] = (self.__basket_left[0] - mean_x) / stddev_x
-            self.__basket_right[0] = (self.__basket_right[0] - mean_x) / stddev_x
-            self.__norm_dict['x'] = {}
-            self.__norm_dict['x']['mean'] = mean_x
-            self.__norm_dict['x']['stddev'] = stddev_x
-            # Y
-            mean_y = np.mean(
-                real_data[:, :, [1, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]])
-            stddev_y = np.std(
-                real_data[:, :, [1, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]])
-            real_data[:, :, [1, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]] = (
-                real_data[:, :, [1, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]] - mean_y) / stddev_y
-            self.__basket_left[1] = (self.__basket_left[1] - mean_y) / stddev_y
-            self.__basket_right[1] = (self.__basket_right[1] - mean_y) / stddev_y
-            self.__norm_dict['y'] = {}
-            self.__norm_dict['y']['mean'] = mean_y
-            self.__norm_dict['y']['stddev'] = stddev_y
-            # Z
-            mean_z = np.mean(
-                real_data[:, :, 2])
-            stddev_z = np.std(
-                real_data[:, :, 2])
-            real_data[:, :, 2] = (
-                real_data[:, :, 2] - mean_z) / stddev_z
-            self.__norm_dict['z'] = {}
-            self.__norm_dict['z']['mean'] = mean_z
-            self.__norm_dict['z']['stddev'] = stddev_z
+            # position normalization
+            self.normalize_pos()
+            # player position encoding
+            self.encode_10_onehot()
 
     def get_normed_data(self):
         return self.__real_data
@@ -74,8 +56,8 @@ class Norm(object):
             self.__norm_dict['z']['stddev'] + self.__norm_dict['z']['mean']
         return samples
 
-    def extract_features(t_data):
-        """ extract 252 features from raw data, including 
+    def extract_features(self, t_data):
+        """ extract 252 features from raw data, including
         * x y z positions = 23
         * x y z speed = 23, first frame's speed should be zero
         * x y correlation of 10 players, ball, and 2 basket = (13*13-13)/2*2=156
@@ -91,26 +73,106 @@ class Norm(object):
             23 = 10 players + ball
             50 = 10 one-hot vector as player position
         """
-        # info
-        t_shape = t_pos.get_shape().as_list()
+        with tf.name_scope('extract') as scope:
+            # info
+            t_shape = t_data.get_shape().as_list()
 
-        # x y z positions = 23
-        t_pos = t_data[:, :, :23]
-        # x y z speed = 23
-        t_speed = tf.stack(
-            [(t_pos[:, 1:100, :] - t_pos[:, 0:99, :]), tf.zeros(shape=[t_shape[0], 1, 23])], axis=1)
-        # x y correlation of 1 ball, 10 players and 2 basket = (13*13-13)/2*2=156
-        t_correlation = []
-        t_basket_l = tf.ones(shape=[t_shape[0], t_shape[1], 1]) * self.__basket_left[0]
-        t_basket_r = tf.ones(shape=[t_shape[0], t_shape[1], 1]) * self.__basket_right[0]
-        t_x = tf.stack([t_pos[:, :, 0], t_pos[:, :, 3::2], t_basket_l, t_basket_r], axis=-1)
-        t_basket_l = tf.ones(shape=[t_shape[0], t_shape[1], 1]) * self.__basket_left[1]
-        t_basket_r = tf.ones(shape=[t_shape[0], t_shape[1], 1]) * self.__basket_right[1]
-        t_y = tf.stack([t_pos[:, :, 1], t_pos[:, :, 4::2], t_basket_l, t_basket_r], axis=-1)
+            # x y z positions = 23
+            t_pos = t_data[:, :, :23]
+            # x y z speed = 23
+            t_speed = tf.concat(
+                [(t_pos[:, 1:100, :] - t_pos[:, 0:99, :]), tf.zeros(shape=[t_shape[0], 1, 23])], axis=1)
+            # x y correlation of 1 ball, 10 players and 2 basket = (13*13-13)/2*2=156
+            t_correlation = []
+            for axis_ in range(2):
+                t_basket_l = tf.ones(
+                    shape=[t_shape[0], t_shape[1], 1]) * self.__basket_left[axis_]
+                t_basket_r = tf.ones(
+                    shape=[t_shape[0], t_shape[1], 1]) * self.__basket_right[axis_]
+                t_x = tf.concat([t_pos[:, :, 0 + axis_:1 + axis_],
+                                 t_pos[:, :, 3 + axis_::2], t_basket_l, t_basket_r], axis=2)
+                for i in range(13):
+                    for j in range(1, i + 1):
+                        t_vec = t_x[:, :, i] - t_x[:, :, j]
+                        t_correlation.append(t_vec)
+            t_correlation = tf.stack(t_correlation, axis=-1)
+            # 10 one-hot vector as player position = 50
+            t_onehot = t_data[:, :, 23:23 + 50]
+            return tf.concat([t_pos, t_speed, t_correlation, t_onehot], axis=-1)
 
-        # 10 one-hot vector as player position = 50
+    def normalize_pos(self):
+        """ directly normalize player x,y,z on self.__real_data
+        """
+        axis_list = ['x', 'y', 'z']
+        for i, axis_ in enumerate(axis_list):
+            if axis_ == 'z':  # z
+                mean_ = np.mean(
+                    self.__real_data[:, :, 0, i])
+                stddev_ = np.std(
+                    self.__real_data[:, :, 0, i])
+                self.__real_data[:, :, 0, i] = (
+                    self.__real_data[:, :, 0, i] - mean_) / stddev_
+                self.__norm_dict[axis_] = {}
+                self.__norm_dict[axis_]['mean'] = mean_
+                self.__norm_dict[axis_]['stddev'] = stddev_
+            else:  # x and y
+                mean_ = np.mean(
+                    self.__real_data[:, :, :, i])
+                stddev_ = np.std(
+                    self.__real_data[:, :, :, i])
+                self.__real_data[:, :, :, i] = (
+                    self.__real_data[:, :, :, i] - mean_) / stddev_
+                self.__basket_left[i] = (
+                    self.__basket_left[i] - mean_) / stddev_
+                self.__basket_right[i] = (
+                    self.__basket_right[i] - mean_) / stddev_
+                self.__norm_dict[axis_] = {}
+                self.__norm_dict[axis_]['mean'] = mean_
+                self.__norm_dict[axis_]['stddev'] = stddev_
+
+    def encode_10_onehot(self):
+        """ directly add player positions one-hot vec on self.__real_data
+
+        note
+        ----
+        player position :
+            * BALL <-> 0
+            * SF <-> 1
+            * PF <-> 2
+            * C <-> 3
+            * PG <-> 4
+            * SG <-> 5
+        """
+        player_position = self.__real_data[:, :,
+                                           1:, -1].astype(np.int)  # without ball
+        ten_onehot = np.zeros(
+            shape=[player_position.shape[0], player_position.shape[1], 10, 5])
+        for i in range(player_position.shape[0]):
+            for j in range(player_position.shape[1]):
+                for k in range(player_position.shape[2]):
+                    ten_onehot[i, j, k, player_position[i][j][k] - 1] = 1
+                    
+        self.__real_data = np.concatenate(
+            [
+                # ball
+                self.__real_data[:, :, 0, :3].reshape(
+                    [self.__real_data.shape[0], self.__real_data.shape[1], 1 * 3]),
+                # players
+                self.__real_data[:, :, 1:, :2].reshape(
+                    [self.__real_data.shape[0], self.__real_data.shape[1], 10 * 2]),
+                # player position as ten 5-dims-one-hot vector
+                ten_onehot.reshape(
+                    [player_position.shape[0], player_position.shape[1], 10 * 5])
+            ], axis=-1
+        )
+
+
+def testing():
+    dummy = np.ones(shape=[512, 100, 11, 4])
+    normer = Norm(dummy)
+    dummy_samples = tf.ones(shape=[512, 100, 23 + 50])
+    normer.extract_features(dummy_samples)
 
 
 if __name__ == '__main__':
-    Norm()
-    Norm()
+    testing()
